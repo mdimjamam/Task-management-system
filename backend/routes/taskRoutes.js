67448bb85cded4bd357bd2e248aaaba4d7fc248d
Md Imjamam <mdinjamam993927@gmail.com>
2026-08-15 14:54:19 +0530

    Remove comments from task routes

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -9,21 +9,12 @@ const {
     deleteTask
 } = require("../controllers/taskController");
 
-
-// Get all tasks
 router.get("/", getTasks);
 
-
-// Create task
 router.post("/", createTask);
 
-
-// Update task
 router.put("/:id", updateTask);
 
-
-// Delete task
 router.delete("/:id", deleteTask);
-
 
 module.exports = router;
