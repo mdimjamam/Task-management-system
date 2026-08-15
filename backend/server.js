b66ba9c38c6658d8dd80de0256cc43e671605533
Md Imjamam <mdinjamam993927@gmail.com>
2026-08-15 14:59:39 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,14 +7,11 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
 app.use("/api/tasks", taskRoutes);
 
-// Test route
 app.get("/", (req, res) => {
     res.json({
         success: true,
@@ -22,7 +19,6 @@ app.get("/", (req, res) => {
     });
 });
 
-// Start server
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on port ${PORT}`);
 });
